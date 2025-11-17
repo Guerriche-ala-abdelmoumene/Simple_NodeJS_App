dfb306c324fce492b50b23d1c0caef102e0c0d4d
Guerriche alla abdelmoumen <147427922+Guerriche-ala-abdelmoumene@users.noreply.github.com>
2025-11-17 17:04:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-  res.send('Hello, World! Welcome to iQuant YouTube Channel.');
+  res.send('Hello, World! Welcome in Alla\'s life.');
 });
 
 app.listen(port, () => {
